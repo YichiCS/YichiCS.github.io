935ebb2f74f13f7bf095b2d9ba7f7aa9e347ed0f
Yichi Zhang <128054771+YichiCS@users.noreply.github.com>
2025-04-30 20:45:24 +0800

    init

--- assets/js/updateDate.js
+++ assets/js/updateDate.js
@@ -1,17 +1,13 @@
 // asserts/js/updateDate.js
 
 function updateTimestamp() {
-    const months = [
-      "January", "February", "March", "April", "May", "June",
-      "July", "August", "September", "October", "November", "December"
-    ];
   
     const currentDate = new Date();
-    const month = months[currentDate.getMonth()];
+    const month = currentDate.getMonth();
     const day = currentDate.getDate();
     const year = currentDate.getFullYear();
   
-    document.getElementById("updateDate").textContent = `${month} ${day}, ${year}`;
+    document.getElementById("updateDate").textContent = `${month}-${day}-${year}`;
   }
   
   document.addEventListener("DOMContentLoaded", updateTimestamp);
