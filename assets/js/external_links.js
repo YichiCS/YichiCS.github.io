9bcef3d99cf8297eb8cfc9a34add5ad438f07b92
Yichi Zhang <128054771+YichiCS@users.noreply.github.com>
2025-05-08 20:13:48 +0800

    new style

--- assets/js/external_links.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('a[href^="http"]').forEach(link => {
-    if (!link.href.includes(window.location.hostname)) {
-      link.setAttribute('target', '_blank');
-      link.setAttribute('rel', 'noopener noreferrer');
-    }
-  });
